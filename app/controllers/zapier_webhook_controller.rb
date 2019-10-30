76a6ce029d72cccacd2e82a968a043d3dc0fe174
Sebastian King <sebastianking@Sebastians-MacBook-Pro.local>
2019-10-30 15:57:25 -0400

    return 200 and turn csrf off

--- app/controllers/zapier_webhook_controller.rb
+++ app/controllers/zapier_webhook_controller.rb
@@ -1,7 +1,13 @@
 class ZapierWebhookController < ApplicationController
+    protect_from_forgery with: :null_session
+
     def recieveWebHook
         begin 
+            # byebug
+            render :json => {:status => 200}
         rescue
+            binding.pry
+            render :json => {:status => 400}
         end
     end
 end

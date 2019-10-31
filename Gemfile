6b1529c2d2fbf33608a6194ef88f0e49878dd31d
Garry Clerge <clergeg@gmail.com>
2019-10-31 08:07:47 -0400

    adds spotify auth still missing path for a new refresh token

--- Gemfile
+++ Gemfile
@@ -56,3 +56,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "react-rails", "~> 2.6"
 gem 'faraday'
 gem 'sidekiq'
+gem 'rest-client'
---
+++ app/controllers/spotify_controller.rb
@@ -0,0 +1,43 @@
+class SpotifyController < ApplicationController
+
+  def authorize
+    body = {
+      client_id: '',
+      client_secret: '',
+      code: params[:code],
+      redirect_uri: 'https://c4698f32.ngrok.io/spotify_auth',
+      grant_type: 'authorization_code',
+    }
+
+    # conn = Faraday.new(:url => 'https://accounts.spotify.com/api/token/')
+    # stuff = conn.post do |req|
+    #   req.headers['Content-Type'] = 'application/json'
+    #   req.body = body
+    # end
+
+    resp = RestClient.post('https://accounts.spotify.com/api/token', body)
+    auth_params = JSON.parse(resp.body)
+    byebug
+
+
+
+  end
+
+  def create
+    query_params = {
+      client_id: '',
+      response_type: "code",
+      redirect_uri: 'https://c4698f32.ngrok.io/spotify_auth',
+      scope: 'playlist-modify-public',
+      show_dialog: false
+
+    }
+    url = 'https://accounts.spotify.com/authorize/'
+
+    puts 'CREATINGGGGGGGGGGGG'
+
+    redirect_to "#{url}?#{query_params.to_query}"
+  end
+
+
+end

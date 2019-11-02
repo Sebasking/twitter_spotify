505980c9ed887f16643838452f15266abd18c00b
Garry Clerge <clergeg@gmail.com>
2019-11-02 18:52:40 -0400

    adds flatiron images

--- app/javascript/components/BreakThroughApp.js
+++ app/javascript/components/BreakThroughApp.js
@@ -1,12 +1,16 @@
 import React from "react"
 import TracksTable from './TracksTable'
 
+
 class BreakThroughApp extends React.Component {
   render () {
     console.log(this.props)
     return (
       <React.Fragment>
         Hi, this is where all of my app should render, this is the top level component
+        <img src={window.flatiron_logo} />
+        <img src={window.we_of_color_logo} />
+        <img src={window.break_through_logo} />
         <TracksTable tracks={this.props.tracks}/>
       </React.Fragment>
     );
